2e8a649fd80a318b081b47d4d7ff1bce062ed371
Benjamin Billet <contact@benjaminbillet.fr>
2019-07-07 12:47:02 +0200

    Style transfer experiment

--- styletransfer/losses.py
+++ styletransfer/losses.py
@@ -1,10 +1,6 @@
 import tensorflow as tf
 
 
-def clip_0_1(image):
-    return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
-
-
 def style_loss(style_outputs, style_targets, style_weights):
     loss_per_layer = [tf.reduce_mean((style_outputs[name]-style_targets[name])**2) for name in style_outputs.keys()]
     style_loss = tf.reduce_sum(tf.multiply(tf.stack(loss_per_layer, 0), style_weights))
@@ -18,8 +14,8 @@ def content_loss(content_outputs, content_targets, content_weights):
     return content_loss
 
 
-def style_content_loss(style_outputs, style_targets, style_weights, alpha, content_outputs, content_targets, content_weights, beta):
-    return alpha * style_loss(style_outputs, style_targets, style_weights) + beta * content_loss(content_outputs, content_targets, content_weights)
+def style_content_loss(content_outputs, content_targets, content_weights, alpha, style_outputs, style_targets, style_weights, beta):
+    return alpha * content_loss(content_outputs, content_targets, content_weights) + beta * style_loss(style_outputs, style_targets, style_weights)
 
 
 def high_pass_x_y(image):
